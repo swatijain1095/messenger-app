ec36ebaf58a699862dd19d8462b5081b9b7ec707
Swati Jain <jain.swati71@gmail.com>
2024-08-25 20:16:59 +0200

    added select contact functionality

--- src/components/ContactCard/ContactCard.tsx
+++ src/components/ContactCard/ContactCard.tsx
@@ -6,12 +6,32 @@ import classNames from "classnames";
 interface ContactCardProps {
   contact: UserI;
   className?: string;
+  onSelectContact?: (contact: UserI) => void;
+  isActive?: boolean;
 }
 
-const ContactCard = ({ contact, className }: ContactCardProps) => {
+const ContactCard = ({
+  contact,
+  className,
+  onSelectContact,
+  isActive,
+}: ContactCardProps) => {
   const { name, image } = contact;
+
+  const handleSelect = () => {
+    if (onSelectContact) {
+      onSelectContact(contact);
+    }
+  };
   return (
-    <div className={classNames("contact-card", className)}>
+    <div
+      className={classNames(
+        "contact-card",
+        isActive && "contact-card--active",
+        className
+      )}
+      onClick={handleSelect}
+    >
       <img className="contact-card__image" src={image} alt={name} />
       <span className="contact-card__name">{name}</span>
     </div>
--- src/components/NavigationPanel/NavigationPanel.tsx
+++ src/components/NavigationPanel/NavigationPanel.tsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./style.scss";
 import { UserI } from "../../types";
 import { getContactList } from "../../api";
 import ContactCard from "../ContactCard/ContactCard";
+import { ContactContext } from "../../context";
 
 const NavigationPanel = () => {
   const [contacts, setContacts] = useState<UserI[]>([]);
+  const { selectedContact, setSelectedContact } = useContext(ContactContext);
 
   useEffect(() => {
     const fetchContactList = async () => {
@@ -20,12 +22,23 @@ const NavigationPanel = () => {
     fetchContactList();
   }, []);
 
+  const handleSelectContact = (contact: UserI) => {
+    setSelectedContact(contact);
+  };
+
   return (
     <div className="navigation-panel">
       <header className="navigation-panel__header">Messenger App</header>
       <div className="navigation-panel__contacts">
         {contacts.map((contact) => {
-          return <ContactCard key={contact.id} contact={contact} />;
+          return (
+            <ContactCard
+              key={contact.id}
+              contact={contact}
+              onSelectContact={handleSelectContact}
+              isActive={selectedContact?.id === contact.id}
+            />
+          );
         })}
       </div>
     </div>
